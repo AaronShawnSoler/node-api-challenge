c2964c151e570cfa1e967d8d325d3d554138d427
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-01-17 11:20:20 -0500

    base endpoints

--- index.js
+++ index.js
@@ -18,6 +18,54 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+// ACTION ENDPOINTS
+server.get('/api/', (req, res) => {
+
+});
+
+server.get('/api/', (req, res) => {
+
+});
+
+server.post('/api/', (req, res) => {
+
+});
+
+server.put('/api/', (req, res) => {
+
+});
+
+server.delete('/api/', (req, res) => {
+
+});
+
+
+//PROJECT ENDPOINTS
+server.get('/api/', (req, res) => {
+
+});
+
+server.get('/api/', (req, res) => {
+
+});
+
+server.post('/api/', (req, res) => {
+
+});
+
+server.put('/api/', (req, res) => {
+
+});
+
+server.delete('/api/', (req, res) => {
+
+});
+
+server.get('/api/', (req, res) => {
+
+});
+
+// Middleware
 
 
 const port = process.env.PORT || 8000;
