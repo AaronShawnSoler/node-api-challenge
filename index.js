df9ba4e36555ebffe5e094d8e4622f57ced0ab01
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-01-17 13:55:04 -0500

    cors fix

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ Go code!
 */
 
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 const projectDB = require('./data/helpers/projectModel');
 const actionDB = require('./data/helpers/actionModel');
